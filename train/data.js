e95ccbd87702fed43a72dcb4b2d31b506ab4938d
AbelCodeUp <bxk0129@163.com>
2023-03-04 19:09:12 +0800

    update

--- train/data.js
+++ train/data.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
-const tf = require("@tensorflow/tfjs-node");
+const tf = require("@tensorflow/tfjs-node-gpu");
 
-const img2x = (buffer) => {
+const img2x = (imgPath) => {
+	const buffer = fs.readFileSync(imgPath);
 	return tf.tidy(() => {
 		const imgTs = tf.node.decodeImage(new Uint8Array(buffer));
 		// 要求224， 224
@@ -18,26 +19,49 @@ const getData = async (trainDir, outputDir) => {
 	const classes = fs.readdirSync(trainDir).filter( n => !n.includes('DS_Store'));
 	fs.writeFileSync(`${outputDir}/classes.json`, JSON.stringify(classes));
 
-    const inputs = [];
-    const labels = [];
+	const data = [];
 	classes.forEach((dir, dirIndex) => {
 		fs.readdirSync(`${trainDir}/${dir}`)
 			.filter((n) => n.match(/(jpg|jpeg|png)$/))
-			.slice(0, 5)
+			.slice(0, 2)
 			.forEach((filename) => {
 				const imgPath = `${trainDir}/${dir}/${filename}`;
-				const buffer = fs.readFileSync(imgPath);
-                const x = img2x(buffer);
-                inputs.push(x);
-                labels.push(dirIndex);
+				data.push({imgPath, dirIndex});
 			});
 	});
 
-    const xs = tf.concat(inputs);
-    const ys = tf.tensor(labels);
+	tf.util.shuffle(data);
+
+	const ds = tf.data.generator(function* () {
+		const count = data.length;
+		const batchSize = 1;
+		for (let start = 0; start < count; start += batchSize) {
+			const end = Math.min(start + batchSize, count);
+			console.log('当前批次', start);
+			yield tf.tidy(() => {
+				const inputs = [];
+				const labels = [];
+				for (let j = 0; j < end; j += 1) {
+					const {imgPath, dirIndex} = data[j];
+					const x = img2x(imgPath);
+					inputs.push(x);
+					labels.push(dirIndex);
+				}
+				const xs = tf.concat(inputs);
+    			const ys = tf.tensor(labels);
+				return {
+					xs,
+					ys
+				}
+			});
+		}
+	})
+
+
+
+
     return {
-        xs,
-        ys,
+		ds,
         classes
     }
 };
--- train/index.js
+++ train/index.js
@@ -1,4 +1,4 @@
-const tf = require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs-node-gpu');
 const getData = require('./data');
 
 const TRAIN_DIR = '垃圾分类';
@@ -6,7 +6,7 @@ const OUTPUT_DIR = 'output';
 const MOBILE_URL = 'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_1.0_224/model.json';
 const main = async () => {
     // 加载数据
-    const { xs, ys, classes } = await getData(TRAIN_DIR, OUTPUT_DIR);
+    const { ds, classes } = await getData(TRAIN_DIR, OUTPUT_DIR);
     // 定义模型 截断模型 + 双层神经网络 （迁移训练）
     const mobilenet = await tf.loadLayersModel(MOBILE_URL);
     mobilenet.summary();
@@ -21,12 +21,21 @@ const main = async () => {
     model.add(tf.layers.dense({
         units: 10,
         activation: 'relu',
-    }))
+    }));
     model.add(tf.layers.dense({
         units: classes.length,
         activation: 'softmax'
-    }))
+    }));
     // 训练模型 损失函数 优化器
+    model.compile({
+        loss: 'sparseCategoricalCrossentropy',
+        optimizer: tf.train.adam(),
+        metrics: ['acc']
+    });
+    await model.fitDataset(ds, {
+        epochs: 20
+    });
+    await model.save(`file://${process.cwd()}/${OUTPUT_DIR}`);
 };
 
 main();
